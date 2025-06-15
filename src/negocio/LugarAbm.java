de02e899ed4c49c62eb1faa45ef1dbb1e3e135fc
Nafer <na_fernandez@live.com.ar>
2025-06-14 23:36:56 -0300

    lugar abm ok

--- src/negocio/LugarAbm.java
+++ src/negocio/LugarAbm.java
@@ -1,65 +1,66 @@
 package negocio;
 
+import java.sql.Time;
 import java.util.List;
 
 import dao.DireccionDao;
 import dao.LocalidadDao;
+import dao.LugarDao;
 import dao.ProvinciaDao;
 import datos.Direccion;
 import datos.Localidad;
+import datos.Lugar;
 import datos.Provincia;
 
 public class LugarAbm {
      LocalidadDao dao= new LocalidadDao();
+     LugarDao lugarDao= new LugarDao();
      ProvinciaDao provinciaDao= new ProvinciaDao();
      DireccionDao direccionDao= new DireccionDao();
   
 
-   	 public Direccion traer(long id) {
-   		 return direccionDao.traer(id);
+   	 public Lugar traer(long id) {
+   		 return lugarDao.traer(id);
    	 }
    	 
-   	 public long agregar(Integer altura, String calle, long idLocalidad) {
-   		Localidad localidad = dao.traerLocalidadYProvincia(idLocalidad); // buscás la provincia por ID
+   	 
+   	 
+   	 public long agregar(Time horarioApertura, Time horarioCierre, long idDireccion) {
+   		 Direccion direccion =direccionDao.traer(idDireccion);
+   		 
+   		List<Lugar>lugares = lugarDao.existeDireccionConLugar(horarioApertura,idDireccion);
+   		if (!lugares.isEmpty()) {
+   		    throw new IllegalArgumentException("Ya existe un Lugar con esa dirección");
+   		}
+   		 
    	
-   	    if (localidad == null) {
-   	        System.err.println("Error: No existe Localidad con ID " + idLocalidad);
-   	        throw new IllegalArgumentException("Provincia no encontrada");
+   	    if (direccion == null) {
+   	        System.err.println("Error: No existe Direccion con ID " );
+   	        throw new IllegalArgumentException("Direccion no encontrada");
    	     
    	    }
-   	    List<Direccion> direcciones = direccionDao.existeDireccionConNombre(calle, altura);
-   	 	if (!direcciones.isEmpty()) {
-     	//Verifica si ya existe una provincia con ese nombre o duplicado
-         System.err.println("Error: Ya existe una Direccion con el nombre: " + calle);
-         //Imprime el errro por consola en rojo
-         throw new IllegalArgumentException("Ya existe una direccion con el nombre: " + calle);
-         //crea la exepcion;
-      }
   
-
-   	        Direccion c = new Direccion();
-   	        c.setCalle(calle);
-   	        c.setLocalidad(localidad);
-   	        c.setProvincia(localidad.getProvincia());
-   	        c.setAltura(altura);
-   	        return direccionDao.agregar(c);
+   	    	Lugar c= new Lugar();
+   	    	c.setDireccion(direccion);
+   	    	c.setHorarioApertura(horarioApertura);
+   	    	c.setHorarioCierre(horarioCierre);
+   	    	c.setServicios(null);
+   	    	return lugarDao.agregar(c);
+   	       
    	    }
    	 
-   	public List<Direccion> traer() {
-		return direccionDao.traer();
+   	public List<Lugar> traer() {
+		return lugarDao.traer();
 		
 	}
-   	public Direccion traerDireccionLocalidadYProvincia(long idLocalidad) {
-   		return direccionDao.traerDireccionLocalidadYProvincia(idLocalidad);
+   	public Lugar traerLugarYdireccion(long idLugar) {
+   		return lugarDao.traerLugarYDireccion(idLugar);
    	}
-   	public Localidad traerLocalidadyProvincia(long id) {
-   		return dao.traerLocalidadYProvincia(id);
-   	}
-   	
-   	 public void modificar(Direccion c) {
-   		direccionDao.actualizar(c);
+   
+   	 public void modificar(Lugar c) {
+   		lugarDao.actualizar(c);
    		}
-   	public void eliminar(Direccion c) {
-   		direccionDao.eliminar(c);
+   	public void eliminar(Lugar c) {
+   		lugarDao.eliminar(c);
    	}
 }
